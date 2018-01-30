aaaac5308ac8ffdd41fd7ba8ff3a2cdbf1e56ddf
Danny Akimchuk <dakimchuk@ithaca.edu>
2018-01-30 15:09:43 -0500

    Test for add one. -dpa

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,12 +1,35 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testAddEmpty() {
         StringCalculator calc = new StringCalculatorImpl();
 
         int sum = calc.add("");
         assertEquals(0, sum, "Expected: 0 Actual: " + sum);
+    }
+
+    @Test
+    void testAddOne() {
+        StringCalculator calc = new StringCalculatorImpl();
+
+        //positive
+        int sum = calc.add("7");
+        assertEquals(7, sum, "Expected: 7 Actual: " + sum);
+
+        //zero
+        sum = calc.add("0");
+        assertEquals(0, sum, "Expected: 0 Actual: " + sum);
+
+        //negative
+        sum = calc.add("-8");
+        assertEquals(-8, sum, "Expected: -8 Actual: " + sum);
+
+        //multiple digits
+        sum = calc.add("123");
+        assertEquals(123, sum, "Expected: 123 Actual: " + sum);
     }
 }
